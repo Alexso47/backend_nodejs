919c490c42af0778253038ff3de9988b6afb331f
Alexso47 <alekox97@gmail.com>
2021-03-27 20:52:42 +0100

    Notes_app functionality before this is within index_deployed.js, commented and deployed on heroku

--- index_deployed.js
+++ index_deployed.js
@@ -1,5 +1,9 @@
 require('./mongo')
 
+/* ---------------- Sentry features ---------------*/
+// const Sentry = require("@sentry/node")
+// const Tracing = require("@sentry/tracing")
+
 // Por defecto configura que cualquier origen puede acceder a la api
 const express = require('express')
 const app = express()
@@ -17,6 +21,8 @@ require('dotenv').config()
 // Middlewares
 const handleErrors = require('./middleware/handleErrors')
 const notFound = require('./middleware/notFound')
+// const logger = require('./middleware/logger')
+
 
 // Models
 const Note = require('./models/Note')
@@ -27,6 +33,31 @@ app.use(express.json())
 app.use(express.static('build'))
 app.use(express.static('images'))
 
+/* ---------------- Sentry features ---------------*/
+// Sentry.init({
+//     dsn: "https://996346bea74144dab72e5b145f5acbc4@o538633.ingest.sentry.io/5656948",
+//     integrations: [
+//         // enable HTTP calls tracing
+//         new Sentry.Integrations.Http({ tracing: true }),
+//         // enable Express.js middleware tracing
+//         new Tracing.Integrations.Express({ app }),
+//     ],
+
+//     // We recommend adjusting this value in production, or using tracesSampler
+//     // for finer control
+//     tracesSampleRate: 1.0,
+// });
+
+// // RequestHandler creates a separate execution context using domains, so that every
+// // transaction/span/breadcrumb is attached to its own Hub instance
+// app.use(Sentry.Handlers.requestHandler());
+// // TracingHandler creates a trace for every incoming request
+// app.use(Sentry.Handlers.tracingHandler());
+
+
+
+// Logger middleware
+// app.use(logger)
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1>')
@@ -87,6 +118,9 @@ app.put('/api/notes/:id', (request, response, next) => {
 
 app.use(notFound)
 
+// // The error handler must be before any other error middleware and after all controllers
+// app.use(Sentry.Handlers.errorHandler());
+
 app.use(handleErrors)
 // Cuando se usa un next() sin parametro error entra en el middleware notfound.
 // Eso quiere decir que no ha hecho match con ningun controlador y no ha habido error en ellas.
@@ -94,9 +128,25 @@ app.use(handleErrors)
 // el error como parametro
 
 
+// Usando Env (valor 3001 por defecto si no encuentra la variable PORT)
+// const PORT = env.get("PORT", 3001)
+
 // De esta manera devuelve el valor que primero encuentre
 const PORT = env.first(["PORT", "HTTP_PORT"], 8080);
+
+// Usando dotenv
+// const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log('Server running on port', PORT)
 })
+
+// const http = require('http')
+// const app = http.createServer((request, response) => {
+//     response.writeHead(200, { 'Content-type': 'application/json '})
+//     response.end(JSON.stringify(notes))
+// })
+
+// app.listen(PORT)
+// console.log('Server running on port', PORT)
+
