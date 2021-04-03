6b94e948bfe169be59a6bd4902170b03d3659823
Alexso47 <alekox97@gmail.com>
2021-04-03 08:57:06 +0200

    Use of env library to get secret when signing a token

--- utils/config.js
+++ utils/config.js
@@ -7,10 +7,12 @@ const PORT = env.first(["PORT", "HTTP_PORT"], 8080);
 const MONGO_DB_URI = env.get("MONGO_DB_URI")
 const MONGO_DB_URI_TEST = env.get("MONGO_DB_URI_TEST")
 const NODE_ENV = env.get("NODE_ENV")
+const SECRET = env.get("SECRET")
 
 module.exports = {
     PORT,
     MONGO_DB_URI,
     MONGO_DB_URI_TEST,
-    NODE_ENV
+    NODE_ENV,
+    SECRET
 }
